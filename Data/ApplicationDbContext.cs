aca8e706e057c40228f56734c5e720f59fe397c9
Nripesh Pokala <nripeshd.ace@gmail.com>
2023-08-13 21:05:23 -0600

    First Commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JokesApp.Models;
+
+namespace JokesApp.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<JokesApp.Models.Joke> Joke { get; set; } = default!;
+}
